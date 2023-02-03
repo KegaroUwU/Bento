9e67341ed8c747b65eaa43a28bc0a4d1f4927da4
KegaroUwU <85635677+KegaroUwU@users.noreply.github.com>
2023-02-02 19:04:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'NoMansSky',
+			icon: 'navigation-2',
+			link: 'https://nomanssky.fandom.com/wiki/No_Man%27s_Sky_Wiki',
 		},
 		{
 			id: '6',
